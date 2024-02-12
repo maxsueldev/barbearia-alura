be05eb59c3f6a50849287006325134a717f65d89
unknown <maxsuel.dev@gmail.com>
2024-02-12 14:35:37 -0300

    responsive mobile

--- js/script.js
+++ js/script.js
@@ -2,9 +2,5 @@ const menuHamburger = document.querySelector('#menu-hamburger');
 const listaHamburger = document.querySelector('#lista-hamburger');
 
 menuHamburger.addEventListener('click', () => {
-    if(listaHamburger.style.display == 'block') {
-        listaHamburger.style.display = 'none';
-    } else {
-        listaHamburger.style.display = 'block';
-    }
+    listaHamburger.classList.toggle('active');
 });
